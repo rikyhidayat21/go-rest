97069ca44cfd7f009a75b95f19275c3f1f9ce9cf
rikyhidayat21 <riky.hidayat@mekari.com>
2023-10-21 06:04:42 +0700

    add test

--- test/simple_service_test.go
+++ test/simple_service_test.go
@@ -1,1 +1,12 @@
 package test
+
+import (
+	"fmt"
+	"memetpaten/go-rest/simple"
+	"testing"
+)
+
+func TestSimpleService(t *testing.T) {
+	simpleService := simple.InitializeService()
+	fmt.Println(simpleService.SimpleRepository)
+}
